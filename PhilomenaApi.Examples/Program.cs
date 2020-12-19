17d0314074811bf33621f04511d2b5c7882942f2
Sibusten <Sibusten@gmail.com>
2020-12-18 21:14:58 -0500

    Copy API examples from philomena-client

--- PhilomenaApi.Examples/Program.cs
+++ PhilomenaApi.Examples/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace PhilomenaApi.Examples
+namespace Philomena.Api.Examples
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IExample> examples = new List<IExample>
+            {
+                new SearchImages(),
+                new SearchTags(),
+            };
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Philomena API examples");
+
+                // Write the list of examples
+                for (int i = 0; i < examples.Count; i++)
+                {
+                    Console.WriteLine($"  {i:#0} - {examples[i].Description}");
+                }
+
+                // Write the quit option
+                Console.WriteLine($"  {examples.Count} - Quit");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter the number of the example to run: ");
+                string input = Console.ReadLine();
+
+                // Parse the input and validate
+                if (!int.TryParse(input, out int choice))
+                {
+                    Console.WriteLine($"Invalid input: '{input}'");
+                    continue;
+                }
+
+                // Ensure the choice is in range
+                if (choice < 0 || choice > examples.Count)
+                {
+                    Console.WriteLine($"Invalid choice: {choice}");
+                    continue;
+                }
+
+                // Check for the quit choice
+                if (choice == examples.Count)
+                {
+                    return;
+                }
+
+                // Run the example
+                IExample exampleToRun = examples[choice];
+                Console.WriteLine();
+                Console.WriteLine("----");
+                Console.WriteLine(exampleToRun.Description);
+                Console.WriteLine("----");
+                Console.WriteLine();
+                await exampleToRun.RunExample();
+            }
         }
     }
 }
